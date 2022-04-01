4a4d385b660c3034002dd053055b407a044ccb29
muslim0792 <abdullayevmuslim92@gmail.com>
2022-04-01 16:50:41 +0400

    Homework 31.03.22

---
+++ PythonWorkingWithData/Task.py
@@ -0,0 +1,57 @@
+#quote daxilində necə xarakter olduğunu tapın 
+
+#quote = "Programming isn't about what you know; it's about what you can figure out."
+#print(len(quote))
+
+################################################################################################################
+
+#quote daxilində necə boşluq olduğunu tapın link
+#def  bosluq_tap(string)
+     #count = 0
+     #for i in range(0, len(string));
+     #if string[1] == " "
+       # count+=1
+     #return count
+ #string = "Programming isn't about what you know; it's about what you can figure out"
+# print("bosluq sayi",bosluq_tap(string))
+
+################################################################################################################
+
+#quote ifadəsini sözlərini ayrı ayrı ekrana çap edin link
+
+#quote = "Programming isn't about what you know; it's about what you can figure out."
+
+#print(quote.split())
+
+
+################################################################################################################
+
+#quote daxilində olan ' işarəsini silərək yeni əldə edilən ifadəni ekrana çap edin link
+
+#quote = "Programming isn't about what you know; it's about what you can figure out."
+
+#x = quote.replace("'", " ")
+
+#print(x)
+
+################################################################################################################
+
+#quote daxilində necə hərf olduğunu tapın link
+
+#quote = "Programming isn't about what you know; it's about what you can figure out."
+
+
+
+
+
+
+
+
+################################################################################################################
+
+
+#quote daxilində necə ədəd o hərfi olduğunu tapın
+
+#quote = "Programming isn't about what you know; it's about what you can figure out"
+#counter = Counter(quote)
+#print(counter['o']) 
